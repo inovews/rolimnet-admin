a447ee3ef2cdd952939477b8a9778630c939b1f5
Inove Ws - Paulo Ricardo <inovewebsites.com@gmail.com>
2017-03-02 14:36:30 -0400

    Add Heroku creds and update default route to hit the DB.

--- config/database.php
+++ config/database.php
@@ -61,11 +61,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host'      => $host,
+        'database'  => $database,
+        'username'  => $username,
+        'password'  => $password,
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
