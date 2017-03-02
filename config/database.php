b9127e474cf9e626287c455e395327d685a90595
Inove Ws - Paulo Ricardo <inovewebsites.com@gmail.com>
2017-03-02 14:14:01 -0400

    Add Heroku creds and update default route to hit the DB.

--- config/database.php
+++ config/database.php
@@ -44,6 +44,13 @@ return [
     |
     */
 
+    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+    $host = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $database = substr($url["path"], 1);
+
     'connections' => [
 
         'sqlite' => [
@@ -54,11 +61,15 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            /*'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'password' => env('DB_PASSWORD', ''),*/
+            'host'      => $host,
+            'database'  => $database,
+            'username'  => $username,
+            'password'  => $password,
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
