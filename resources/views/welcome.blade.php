52a15c78813ab2003fadf5ee6e4cf1636d8b83e7
Inove Ws - Paulo Ricardo <inovewebsites.com@gmail.com>
2017-03-05 16:10:55 -0400

    add news layout rolimnet

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,95 +1,107 @@
 <!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html>
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-        <title>Rolim Net</title>
+    <link href="http://fonts.googleapis.com/css?family=Hind:300,400,500,600,700" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" type="text/css" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/index.css">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <title>Rolim Net</title>
+</head>
+<body>
+    <div id="inicio" class="site">
+        <header class="header navbar-fixed-top">
+            <nav class="navbar" role="navigation">
+                <div class="container">
+                    <div class="menu-container js_nav-item">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".nav-collapse">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="toggle-icon"></span>
+                        </button>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+                        <div class="logo">
+                            <a class="logo-wrap" href="#body">
+                                <img class="logo-img logo-img-main" src="assets/img/logo.png" alt="Rolim Net Logo">
+                                <img class="logo-img logo-img-active" src="assets/img/logo-dark.png" alt="Rolim Net Logo">
+                            </a>
+                        </div>
+                    </div>
 
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
-                    @endif
+                    <div class="collapse navbar-collapse nav-collapse">
+                        <div class="menu-container">
+                            <ul class="nav navbar-nav navbar-nav-right">
+                                <li class="nav-item"><a class="nav-item-child nav-item-hover" href="#inicio">Inicio</a></li>
+                                <li class="nav-item"><a class="nav-item-child nav-item-hover" href="#empresa">Empresa</a></li>
+                                <li class="nav-item"><a class="nav-item-child nav-item-hover" href="#interent">Internet</a></li>
+                                <li class="nav-item"><a class="nav-item-child nav-item-hover" href="#suporte">Suporte</a></li>
+                                <li class="nav-item"><a class="nav-item-child nav-item-hover" href="#contato">Contato</a></li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
-            @endif
+            </nav>
+        </header>
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
+        <div class="promo-block parallax-window" data-parallax="scroll" data-image-src="assets/img/1920x1080/02.jpg">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-6">
+                        <div class="promo-block-divider">
+                            <h1 class="promo-block-title">Paulo <br/> Ricardo</h1>
+                            <p class="promo-block-text">Web Designer &amp; Front-end Developer</p>
+                        </div>
+                    </div>
                 </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <!--// end row -->
+            </div>
+        </div>
+    </div>
+    <div id="empresa">
+        <div class="container content-sm">
+            <div class="row">
+                <div class="col-sm-6">
+                    <div class="text-center sm-text-left">
+                        <h1 class="color-blue margin-b-0">Bem-Vindo!</h1>
+                        <p>A Rolim Net surgiu no ano de 2008, hoje tem presença em mais de 10 cidades e várias Linhas na Zona da Mata. A Rolim Net é ganhadora 
+do Troféu ACIRM dos anos de 2010, 2012, 2013, 2014 e 2015.</p>
+                        <p class="text-right"><a href="#" class="color-blue">...mais sobre a empresa</a></p>
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <div class="empresa-img text-right">
+                        <img class="familia-img" src="assets/img/bg-familia-rolimnet-2016.jpg">
+                    </div>
+                </div>
+            </div>
+            <!--// end row -->
+        </div>
+    </div>
+    <div id="centraldocliente">
+            <div class="bg-color-blue" data-auto-height="true">
+                <div class="container content">
+                    <div class="row">
+                        <div class="col-sm-8">
+                        <button type="button" class="btn btn-default">Acessar o FAQ</button>
+                        </div>
+                        <div class="col-sm-3  col-sm-offset-1 ">
+                            <div class="text-right sm-text-left">
+                                <h2 class="color-white margin-b-0">Central do Cliente</h2>
+                            </div>
+                        </div>
+                    </div>
+                    <!--// end row -->
                 </div>
             </div>
         </div>
-    </body>
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+
+    <script src="assets/js/jquery.min.js" type="text/javascript"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+    <script src="assets/js/jquery.parallax.min.js" type="text/javascript"></script>
+
+    <script src="assets/js/index.js" type="text/javascript"></script>
+        
+</body>
 </html>
