6e328024a796a508c7e4156da4e52aa1b43c14b5
Inove Ws - Paulo Ricardo <inovewebsites.com@gmail.com>
2017-03-05 19:28:57 -0400

    add up index welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,65 +11,75 @@
     <title>Rolim Net</title>
 </head>
 <body>
-    <div id="inicio" class="site">
-        <header class="header navbar-fixed-top">
-            <nav class="navbar" role="navigation">
-                <div class="container">
-                    <div class="menu-container js_nav-item">
-                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".nav-collapse">
-                            <span class="sr-only">Toggle navigation</span>
-                            <span class="toggle-icon"></span>
-                        </button>
-
-                        <div class="logo">
-                            <a class="logo-wrap" href="#body">
-                                <img class="logo-img logo-img-main" src="assets/img/logo.png" alt="Rolim Net Logo">
-                                <img class="logo-img logo-img-active" src="assets/img/logo-dark.png" alt="Rolim Net Logo">
-                            </a>
-                        </div>
-                    </div>
-
-                    <div class="collapse navbar-collapse nav-collapse">
-                        <div class="menu-container">
-                            <ul class="nav navbar-nav navbar-nav-right">
-                                <li class="nav-item"><a class="nav-item-child nav-item-hover" href="#inicio">Inicio</a></li>
-                                <li class="nav-item"><a class="nav-item-child nav-item-hover" href="#empresa">Empresa</a></li>
-                                <li class="nav-item"><a class="nav-item-child nav-item-hover" href="#interent">Internet</a></li>
-                                <li class="nav-item"><a class="nav-item-child nav-item-hover" href="#suporte">Suporte</a></li>
-                                <li class="nav-item"><a class="nav-item-child nav-item-hover" href="#contato">Contato</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </nav>
-        </header>
-
-        <div class="promo-block parallax-window" data-parallax="scroll" data-image-src="assets/img/1920x1080/02.jpg">
+    <header class="header navbar-fixed-top">
+        <nav class="navbar" role="navigation">
             <div class="container">
-                <div class="row">
-                    <div class="col-sm-6">
-                        <div class="promo-block-divider">
-                            <h1 class="promo-block-title">Paulo <br/> Ricardo</h1>
-                            <p class="promo-block-text">Web Designer &amp; Front-end Developer</p>
-                        </div>
+                <div class="menu-container js_nav-item">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".nav-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="toggle-icon"></span>
+                    </button>
+
+                    <div class="logo">
+                        <a class="logo-wrap" href="#body">
+                            <img class="logo-img logo-img-main" src="assets/img/logo.png" alt="Rolim Net Logo">
+                            <img class="logo-img logo-img-active" src="assets/img/logo-dark.png" alt="Rolim Net Logo">
+                        </a>
                     </div>
                 </div>
-                <!--// end row -->
+
+                <div class="collapse navbar-collapse nav-collapse">
+                    <div class="menu-container">
+                        <ul class="nav navbar-nav navbar-nav-right">
+                            <li class="nav-item"><a class="nav-item-child nav-item-hover" href="#inicio">Inicio</a></li>
+                            <li class="nav-item"><a class="nav-item-child nav-item-hover" href="#empresa">Empresa</a></li>
+                            <li class="nav-item"><a class="nav-item-child nav-item-hover" href="#interent">Internet</a></li>
+                            <li class="nav-item"><a class="nav-item-child nav-item-hover" href="#suporte">Suporte</a></li>
+                            <li class="nav-item"><a class="nav-item-child nav-item-hover" href="#contato">Contato</a></li>
+                        </ul>
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-    <div id="empresa">
+        </nav>
+    </header>
+
+    <div class="banner-index" data-parallax="scroll" data-image-src="assets/img/1920x1080/02.jpg">
         <div class="container content-sm">
             <div class="row">
                 <div class="col-sm-6">
+                </div>
+                <div class="col-sm-6">
+                    <div class="planos">
+                        <h2 class="banner-index-planos">3mega DE INTERNET</h2>
+                        <h2 class="banner-index-velocidade">3072k/1024k</h2>
+                        <p class="banner-index-down-up">down/up</p>
+                        
+                        <ul class="text">
+                            <li class="color-white" >Sem limite de navegação</li>
+                            <li class="color-white" >Cobertura fibra óptica</li>
+                        </ul>
+                        <hr>
+                        <h1><span>R$</span> 99,00</h1>
+                        <button type="button" class="btn btn-lg btn-warning">Assine já</button>
+                        <p><a href="#">Conheça outros planos</a></p>
+                    </div>
+                </div>
+            </div>
+            <!--// end row -->
+        </div>
+    </div>
+
+    <div id="empresa">
+        <div class="container content-sm">
+            <div class="row">
+                <div class="col-sm-4 col-lg-offset-1">
                     <div class="text-center sm-text-left">
                         <h1 class="color-blue margin-b-0">Bem-Vindo!</h1>
-                        <p>A Rolim Net surgiu no ano de 2008, hoje tem presença em mais de 10 cidades e várias Linhas na Zona da Mata. A Rolim Net é ganhadora 
-do Troféu ACIRM dos anos de 2010, 2012, 2013, 2014 e 2015.</p>
+                        <p>A Rolim Net surgiu no ano de 2008, hoje tem presença em mais de 10 cidades e várias Linhas na Zona da Mata. A Rolim Net é ganhadora do Troféu ACIRM dos anos de 2010, 2012, 2013, 2014 e 2015.</p>
                         <p class="text-right"><a href="#" class="color-blue">...mais sobre a empresa</a></p>
                     </div>
                 </div>
-                <div class="col-sm-6">
+                <div class="col-sm-7">
                     <div class="empresa-img text-right">
                         <img class="familia-img" src="assets/img/bg-familia-rolimnet-2016.jpg">
                     </div>
@@ -78,30 +88,67 @@ do Troféu ACIRM dos anos de 2010, 2012, 2013, 2014 e 2015.</p>
             <!--// end row -->
         </div>
     </div>
+
     <div id="centraldocliente">
-            <div class="bg-color-blue" data-auto-height="true">
-                <div class="container content">
-                    <div class="row">
-                        <div class="col-sm-8">
+        <div class="bg-color-blue" data-auto-height="true">
+            <div class="container content">
+                <div class="row">
+                    <div class="col-sm-8">
                         <button type="button" class="btn btn-default">Acessar o FAQ</button>
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <h4 class="color-white">Perguntas Frequentes</h4>
+                                <ul class="centraldocliente">
+                                    <li><a class="color-white" href="#">Segunda vida da Fatura.</a></li>
+                                    <li><a class="color-white"href="#">Acabou o carne.</a></li>
+                                    <li><a class="color-white" href="#">Como cancelar minha assinatura.</a></li>
+                                </ul>
+                            </div>
+                            <!--// end row -->
                         </div>
-                        <div class="col-sm-3  col-sm-offset-1 ">
-                            <div class="text-right sm-text-left">
-                                <h2 class="color-white margin-b-0">Central do Cliente</h2>
+                    </div>
+                    <div class="col-sm-4">
+                        <div class="text-right sm-text-left">
+                            <h2 class="color-white margin-b-0">Central do Cliente</h2>
+                            <div class="row">
+                                <div class="col-sm-12">
+                                    <button type="button" class="btn btn-lg btn-default">Acessar Central</button>
+                                </div>
+                                <!--// end row -->
                             </div>
                         </div>
                     </div>
-                    <!--// end row -->
                 </div>
+                <!--// end row -->
             </div>
         </div>
-    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    </div>
+
+    <div id="empresa">
+        <div class="container content-sm">
+            <div class="row">
+            Test
+            </div>
+            <!--// end row -->
+        </div>
+    </div>
+
+    <div class="test" data-parallax="scroll" data-image-src="assets/img/1920x1080/02.jpg">
+        <div class="container content-sm">
+            <div class="row">
+                <div class="col-sm-12">
+                </div>
+            </div>
+            <!--// end row -->
+        </div>
+    </div>
+    <br><br><br><br><br><br><br><br><br>
 
     <script src="assets/js/jquery.min.js" type="text/javascript"></script>
     <script src="assets/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
     <script src="assets/js/jquery.parallax.min.js" type="text/javascript"></script>
 
     <script src="assets/js/index.js" type="text/javascript"></script>
-        
+
 </body>
 </html>
